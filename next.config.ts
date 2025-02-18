979c86186a4e73135503376ada2ed6a2b0e5385c
Mrinmayee Sarkar <mrinmayee.sarkar23@gmail.com>
2025-02-18 20:18:57 +0530

    updated next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Fix for images not loading on Netlify
+  },
 };
 
 export default nextConfig;
